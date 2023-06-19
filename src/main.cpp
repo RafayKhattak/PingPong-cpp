d6a451a89a6ed4e0b1bf383bcb0383d13428ebc2
Rafay Khattak <rafaykhankhattak@gmail.com>
2023-06-19 23:35:12 +0500

    Testing something else

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-// Define colors
+// Define color
 Color Green = Color{38, 185, 154, 255};
 Color Dark_Green = Color{20, 160, 133, 255};
 Color Light_Green = Color{129, 204, 184, 255};
